21a94ef5a23a1e021996bebf262b7c21ce2c0ef8
Christian Tischer <christian.tischer@embl.de>
2019-10-09 09:35:33 +0200

    Bump version and add to bintray

---
+++ src/test/java/command/RunBatchClassifierCommand.java
@@ -0,0 +1,15 @@
+package command;
+
+import de.embl.cba.cats.ui.BatchClassificationCommand;
+import net.imagej.ImageJ;
+
+public class RunBatchClassifierCommand
+{
+    public static void main( final String... args )
+    {
+        final ImageJ ij = new ImageJ();
+        ij.ui().showUI();
+
+        ij.command().run( BatchClassificationCommand.class, true );
+    }
+}
